67c44953e7a539d5a8bb77a69cbbb116101dae30
Serra Allgood <serra@allgood.dev>
2019-11-07 17:50:16 -0800

    Create useIntersection implementation with TypeScript tyings
    Signed-off-by: Serra Allgood <serra@allgood.dev>
    Co-authored-by: Justin Travis Waith-Mair <https://twitter.com/want2code>
    Co-authored-by: Jared Anderson <https://twitter.com/tuxsudo>

---
+++ src/UseIntersection.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+type UseIntersectionArgs = {
+  root: null | Element,
+  rootMargin: string,
+  threshold: number
+}
+
+export const useIntersection = (
+  { root = null, rootMargin, threshold = 0 }: UseIntersectionArgs
+): Array<{} | IntersectionObserverEntry | React.Dispatch<React.SetStateAction<any>>> => {
+  const [entry, setEntry]: [
+    {} | IntersectionObserverEntry,
+    React.Dispatch<React.SetStateAction<any>>
+  ] = useState({})
+  const [node, setNode]: [
+    null | Element,
+    React.Dispatch<React.SetStateAction<any>>
+  ] = useState(null)
+  const observer: React.MutableRefObject<null | IntersectionObserver> = useRef(null)
+
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect()
+
+    observer.current = new IntersectionObserver(
+      ([entry]) => setEntry(entry),
+      { root, rootMargin, threshold }
+    )
+
+    const { current: currentObserver } = observer
+
+    if (node) currentObserver.observe(node)
+
+    return () => currentObserver.disconnect()
+  }, [node, root, rootMargin, threshold])
+
+  return [setNode, entry]
+}
