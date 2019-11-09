1370154ca8a1dda410e59348441b9da3b2df1ed1
Serra Allgood <serra@allgood.dev>
2019-11-09 06:10:55 -0800

    Add basic spec for entry passing.
    Signed-off-by: Serra Allgood <serra@allgood.dev>

---
+++ src/__tests__/UseIntersection.test.tsx
@@ -0,0 +1,54 @@
+import React, { PropsWithChildren, useEffect, useRef } from 'react'
+import ReactTestUtils from 'react-dom/test-utils'
+import { mount } from 'enzyme'
+require('intersection-observer')
+
+import useIntersection from '../'
+
+describe('useIntersection', () => {
+  const windowIntersectionObserver = window.IntersectionObserver
+  let mockCallback: Function
+
+  beforeAll(() => {
+    (window.IntersectionObserver as any) = function (this: IntersectionObserver, callback: Function) {
+      mockCallback = callback
+      this.observe = () => {}
+      this.disconnect = () => {}
+    }
+  })
+
+  afterAll(() => {
+    window.IntersectionObserver = windowIntersectionObserver
+  })
+
+  test('entry passing', () => {
+    const ChildComponent: React.FC<{ entry: IntersectionObserverEntry | null }> = ({ entry }) => (
+      <div id='test-node'>{entry ? entry.intersectionRatio : ''}</div>
+    )
+    const ParentComponent: React.FC<PropsWithChildren<{}>> = () => {
+      const [setNode, entry] = useIntersection()
+      const anchor = useRef(null)
+
+      useEffect(() => {
+        setNode(anchor.current)
+      })
+
+      return (
+        <>
+          <div id='top-of-page-anchor' ref={anchor} />
+          <ChildComponent entry={entry} />
+        </>
+      )
+    }
+    const mockEntry = { intersectionRatio: 0.5, isIntersecting: true } as IntersectionObserverEntry
+    const wrapper = mount(<ParentComponent />)
+
+    expect(wrapper.find('#test-node').text()).toBe('')
+
+    ReactTestUtils.act(() => {
+      mockCallback([mockEntry])
+    })
+
+    expect(wrapper.find('#test-node').text()).toBe('0.5')
+  })
+})
